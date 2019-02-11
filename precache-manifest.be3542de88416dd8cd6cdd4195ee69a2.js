d418db3fcf5b13529a2ed551d26adf5232ea4392
Caio Ferrarezi <caioferrarezi2@gmail.com>
2019-02-11 17:36:43 -0200

    Updates

---
+++ precache-manifest.be3542de88416dd8cd6cdd4195ee69a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13fcbe60c55f37b1c60e",
+    "url": "/seven-clicks/static/js/runtime~main.13fcbe60.js"
+  },
+  {
+    "revision": "d54aabd3cf7ed743ff6d",
+    "url": "/seven-clicks/static/js/main.d54aabd3.chunk.js"
+  },
+  {
+    "revision": "c6f150db1915db0a0e49",
+    "url": "/seven-clicks/static/js/1.c6f150db.chunk.js"
+  },
+  {
+    "revision": "d54aabd3cf7ed743ff6d",
+    "url": "/seven-clicks/static/css/main.443e0b1e.chunk.css"
+  },
+  {
+    "revision": "cb5eacead1f87c826b263376950f998c",
+    "url": "/seven-clicks/index.html"
+  }
+];
